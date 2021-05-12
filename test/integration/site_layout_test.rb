45ec7a66d3aaa42ec778b1fff9aff87d4ebf7cb3
taro1025 <game4967@gmail.com>
2021-05-12 19:33:28 +0000

    Finish user edit, update, index, destrouy actions

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:michael)
+  end  
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
@@ -9,5 +13,19 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
     assert_select "a[href=?]", signup_path
+    p "fafa#{user_path(@user)}"
+    post login_path , params: { session: { email:    @user.email,
+                                          password: 'password' } }
+    get root_path                                          
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", signup_path    
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user)
   end
+  
 end
