aa991ef3af8afbea4c61f69d6c49279c0012b082
taro1025 <game4967@gmail.com>
2021-05-12 09:59:29 +0000

    Implement advanced login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,9 +4,25 @@ module SessionsHelper
     session[:user_id] = user.id
   end
   
+  #クッキーにトークンを保存
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent.signed[:remember_token] = user.remember_token
+  end
+  
   def current_user
-    if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
+    
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      
+      if user && user.authenticated?(cookies.signed[:remember_token])
+        log_in user
+        p "this is current user"
+        @current_user = user
+      end
     end
   end
   
@@ -14,8 +30,16 @@ module SessionsHelper
     !current_user.nil?
   end
   
+  def forget(user)
+    user.forget #データベース側の削除
+    cookies.delete(:user_id) #クッキー側の削除
+    cookies.delete(:remember_token)
+  end
+
+  # 現在のユーザーをログアウトする
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
-  end  
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor  :remember_token
+  
   before_save {self.email.downcase! }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -10,9 +12,30 @@ class User < ApplicationRecord
   validates :password, presence: true, length: { minimum: 6 }
   
   
+  # 渡された文字列のハッシュ値を返す
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
+
+  # ランダムなトークンを返す
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  # データベースにトークンを保存
+  def remember
+    self.remember_token = User.new_token #remember_tokenはインスタンす変数
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # 渡されたクッキーのトークンがデータベースのトークン（ダイジェスト）と一致したらtrueを返す
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+  def forget
+    update_attribute(:remember_digest, nil)
+  end  
 end
